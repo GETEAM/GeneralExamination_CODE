d13b21b095ad40377de9869c0ca0c81429eee9fd
wangxin1990 <825249167@qq.com>
2015-12-02 01:17:12 +0800

    create ExaminationRoomInfomation

---
+++ src/ManageBundle/SystemManagementBundle/ExaminationRoomInfomationBundle/ManageBundleSystemManagementBundleExaminationRoomInfomationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ManageBundle\SystemManagementBundle\ExaminationRoomInfomationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ManageBundleSystemManagementBundleExaminationRoomInfomationBundle extends Bundle
+{
+}
