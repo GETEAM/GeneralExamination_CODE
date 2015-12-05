6a440b8cefd5ee16110e6993a318a3b839dc085a
very-much <fenggao@mail.ustc.edu.cn>
2015-12-05 22:00:31 +0800

    add table tr multiple-selected

--- src/GE/SystemManageBundle/Resources/public/js/grade_index.js
+++ src/GE/SystemManageBundle/Resources/public/js/grade_index.js
@@ -1,14 +1,19 @@
 $(function() {
-	console.log(111);
-	$('.grades tbody tr').click(function() {
-		var $checkbox = $('input[type="checkbox"]', $(this))
-		var checked = $checkbox.attr('checked')
-		$checkbox.attr('checked', !checked);
-		if(!checked) {
-			$(this).addClass('selected');
-		}else {
-			$(this).removeClass('selected');
+	//单击表格tr 选中一行
+	$('.grades tbody tr').click(function(e) {
+		var event_target = e.target;
+		//如果点击的位置为a标签或者父元素部位A标签 则选中该行
+		if( event_target.tagName != 'A' && event_target.parentNode.tagName != 'A' ){
+			var $checkbox = $('input[type="checkbox"]', $(this))
+			var checked = $checkbox.attr('checked')
+			//改变复选框的选中状态
+			$checkbox.attr('checked', !checked);
+			//根据checkbox的选中状态添加删除class
+			if(!checked) {
+				$(this).addClass('selected');
+			}else {
+				$(this).removeClass('selected');
+			}
 		}
-		
 	})
 })
