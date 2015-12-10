1aaf57c434478ada0f1b280a9806524c8e573e5c
wangxin1990 <825249167@qq.com>
2015-12-10 10:00:09 +0800

    update entity name

--- src/GE/UserBundle/Controller/UserController.php
+++ src/GE/UserBundle/Controller/UserController.php
@@ -76,7 +76,7 @@ class UserController extends Controller
             'method' => 'POST',
         ));
 
-        $form->add('submit', 'submit', array('label' => 'Create'));
+        $form->add('submit', 'submit', array('label' => '创建'));
 
         return $form;
     }
@@ -165,7 +165,7 @@ class UserController extends Controller
             'method' => 'PUT',
         ));
 
-        $form->add('submit', 'submit', array('label' => 'Update'));
+        $form->add('submit', 'submit', array('label' => '更新'));
 
         return $form;
     }
@@ -240,7 +240,7 @@ class UserController extends Controller
         return $this->createFormBuilder()
             ->setAction($this->generateUrl('user_delete', array('id' => $id)))
             ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->add('submit', 'submit', array('label' => '删除'))
             ->getForm()
         ;
     }
