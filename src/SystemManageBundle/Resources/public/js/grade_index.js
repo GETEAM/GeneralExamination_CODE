0840b3af090e33b57bef0e6246d00d12f46cb588
dst_program <1258632526@qq.com>
2015-12-21 12:49:56 +0800

    modify single delete

--- src/SystemManageBundle/Resources/public/js/grade_index.js
+++ src/SystemManageBundle/Resources/public/js/grade_index.js
@@ -3,36 +3,9 @@ $(function() {
 	trSelectInitial($('.grades tbody tr'), 'grades');
 
 	/*** 单个删除 ***/
-	// 对话框 初始化
-	$( '.delete-dialog' ).dialog({ 
-		closeOnEscape: true,//按下ESC后是否退出
-		modal: true,//出现模态弹出框
-		resized: false,
-		autoOpen: false,
-		buttons: {
-			'确认': function() {
-				$(this).dialog('close');
-				$('.loading-description').text("正在删除,请稍等……");
-				$('.loading').show();
-				var grade_id = $(this).attr('grade-id');
-				//跳转到删除页面
-				location.href = '/manage/grade/delete/' + grade_id;
-			},
-			'取消': function() {
-				//此处的this为$( '.delete-dialog' )
-				$(this).dialog('close');
-			}
-		}
-	});
-	// 点击删除按钮时，打开删除对话框
-	$('.delete-btn').click(function(){
-		var grade_id = $(this).closest('a').attr('grade-id');
-		//对应的对话框id
-		var dialog_id = '#delete-dialog_' + grade_id;
-		//打开对话框
-		$(dialog_id).dialog('open');
-	});
+	singleDelete('grade');
 
+	
 	/*** 批量删除 ***/	
 	//批量删除对话框初始化
 	$( '.multi-delete-dialog' ).dialog({ 
---
+++ src/UserBundle/Resources/public/js/manager_index.js
@@ -0,0 +1,37 @@
+$(function(){
+
+	singleDelete("manager");
+	
+});
+
+	function singleDelete(name){
+		$( '.delete-dialog' ).dialog({ 
+			closeOnEscape: true,//按下ESC后是否退出
+			modal: true,//出现模态弹出框
+			resized: false,
+			autoOpen: false,
+			buttons: {
+				'确认': function() {
+					$(this).dialog('close');
+					$('.loading-description').text("正在删除,请稍等……");
+					$('.loading').show();
+					var delete_id = $(this).attr(name +'-id');
+					//跳转到删除页面
+					location.href = '/manage/' + name +'/delete/' + delete_id;
+				},
+				'取消': function() {
+					//此处的this为$( '.delete-dialog' )
+					$(this).dialog('close');
+				}
+			}
+		});
+		// 点击删除按钮时，打开删除对话框
+		$('.delete-btn').click(function(){
+			var name_id = $(this).closest('a').attr(name + '-id');
+			//对应的对话框id
+			var dialog_id = '#'+ name +'-delete-dialog_' + name_id;
+			//打开对话框
+			$(dialog_id).dialog('open');
+		});
+		
+	}
