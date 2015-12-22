a43276e0576831fe497cfec0dca37b5cb17d5d49
dst_program <1258632526@qq.com>
2015-12-22 11:52:29 +0800

    modify import form

--- web/js/manage/user-manage/manager_new.js
+++ web/js/manage/user-manage/manager_new.js
@@ -1,5 +1,5 @@
 $(function(){
-	$('.user-add a').click(function(){
+	$('.common-action a').click(function(){
 		if($(this).hasClass('no-selected')){
 			$(this).removeClass('no-selected').addClass('add-selected');
 			$(this).siblings().removeClass('add-selected').addClass('no-selected');
