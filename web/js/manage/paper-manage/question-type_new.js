e719c3af967f67516fddfe0ddf7732c6ce3d32a9
very-much <fenggao@mail.ustc.edu.cn>
2016-01-04 11:29:29 +0800

    new question-type -> react

--- web/js/manage/paper-manage/question-type_new.js
+++ web/js/manage/paper-manage/question-type_new.js
@@ -1,17 +1,301 @@
 $(function(){
-	/*★★★★★★★★★★注意：添加位置如何决定*/
-	/*添加删除试题选项*/
-	//点击添加试题选项时，添加试题选项
-	$('.add-item-option').click(function() {
-		//添加试题选项
-		$(this).addItemOption();
+	// /*★★★★★★★★★★注意：添加位置如何决定*/
+	// /*添加删除试题选项*/
+	// //点击添加试题选项时，添加试题选项
+	// $('.add-item-option').click(function() {
+	// 	//添加试题选项
+	// 	$(this).addItemOption();
+	// });
+
+	// /*添加、删除小题*/
+	// $('.add-single-choice').click(function(){
+	// 	//添加小题
+	// 	$(this).addQuestion();
+	// });
+	
+	var deepCopy= function(source) { 
+		var result={};
+		for (var key in source) {
+	     	result[key] = typeof source[key]==='object' ? deepCopy(source[key]): source[key];
+	   	} 
+	   	return result; 
+	}
+
+	var item = {	
+		"stem": "此处为试题题干内容，题干中可以包括图片、音频以及视频等多媒体。",
+		"length": 300,
+		"showLength": true, 
+		"options": [ 		
+			"试题选项内容",			
+			"试题选项内容",	
+			"试题选项内容",	
+			"试题选项内容",	
+			"试题选项内容",	
+			"试题选项内容",	
+			"试题选项内容"
+		],
+		"shuffle": true,
+		"preShow": true,
+		"questions": [			
+			{
+				"type": "SingleChoice",	
+				"pause": 20, //此处单位为秒
+				"shuffle": true, //只有当type为多选或者单选时，该选项才有效			
+				"stem": "此处为单选小题题干，题干内容可以包括图片、音频以及视频等多媒体。",
+				"options": [		//question的各个选项
+					"单选小题选项内容",
+					"单选小题选项内容",
+					"单选小题选项内容",
+					"单选小题选项内容"
+				],
+				"strict": true,	//正确答案是否要与参考答案完全匹配
+				"reference-answer": "参考答案",		
+				"answer-analysis": "参考答案解析"
+			},
+			{
+				"type": "SingleChoice",	
+				"pause": 20, //此处单位为秒
+				"shuffle": true, //只有当type为多选或者单选时，该选项才有效			
+				"stem": "此处为单选小题题干，题干内容可以包括图片、音频以及视频等多媒体。",
+				"options": [		//question的各个选项
+					"单选小题选项内容",
+					"单选小题选项内容",
+					"单选小题选项内容",
+					"单选小题选项内容"
+				],
+				"strict": true,	//正确答案是否要与参考答案完全匹配
+				"reference-answer": "参考答案",		
+				"answer-analysis": "参考答案解析"
+			}
+		]
+	};
+
+	//试题Item
+	var Item = React.createClass({
+		getInitialState: function(){
+			return {
+				item: item
+			}
+		},
+		handleChangeState: function() {
+			this.setState({
+				item: item
+			});
+		},
+		render: function() {
+			var item = this.state.item;
+
+			var options = item.options;
+
+			//如果item选项存在，则插入ItemOptions
+			var item_options = options ?　<ItemOptions options={options} changeItemState={this.handleChangeState} /> : '';
+
+			//如果存在questions，则插入Question
+			var questions = item.questions ? <Questions questions={item.questions} itemOptions={options} changeItemState={this.handleChangeState}/> : '';
+			
+			return (
+				<div className="item">
+					<ItemStem stem={item.stem}/>
+					{item_options}
+					{questions}
+				</div>
+			);
+		}
 	});
 
-	/*添加、删除小题*/
-	$('.add-single-choice').click(function(){
-		//添加小题
-		$(this).addQuestion();
+	//试题item题干
+	var ItemStem = React.createClass({
+		render: function() {
+			return (
+				<div className="item-stem">
+					<label>题干</label>
+					<div className="item-stem-content">
+						{this.props.stem}
+					</div>
+				</div>
+			);
+		}
 	});
+
+	// 试题item的options
+	var ItemOptions = React.createClass({
+		deleteItemOption: function() {
+			//移除试题选项中的一个
+			item.options.pop();
+			//改变item state
+			this.props.changeItemState();
+		},
+		render: function() {
+			var self = this;
+			var options = this.props.options;
+			return (
+				<div className="item-options">
+	    			<label>试题选项</label>
+	    			<ul className="item-options-list">
+    				{
+    					options.map(function(option) {
+    						return (
+    							<li>
+    								<span>{option}</span>
+    								<a href="javascript:void(0)" className="delete-item-option" onClick={self.deleteItemOption}>
+    									<img src="/images/manage/delete-min.png" width="16" height="16" alt="删除试题选项" title="删除试题选项" />
+    								</a>
+    							</li>
+    						);
+    					})
+    				}
+	    			</ul>
+	    		</div>
+			)
+		}
+	});
+
+	//试题item内的questions
+	var Questions = React.createClass({
+		render: function() {
+			var self = this;
+			var item_options = this.props.itemOptions;
+			var questions = this.props.questions;
+
+			return (
+				<div className="item-questions">
+					<label>包含小题</label>
+					<div className="questions">
+					{
+						 questions.map(function( question, i, questions ){
+							return (
+								<Question question={question} itemOptions={item_options} order={i} changeItemState={self.props.changeItemState} />
+							);
+						})
+					}
+					</div>
+				</div>
+			);
+		}
+	});
+	
+	//试题item中包含的小题Question
+	var Question = React.createClass({
+		addQuestionFromCurrent: function() {
+			var question = this.props.question;
+			var order = this.props.order;
+			//复制当前小题为新小题
+			var new_question = deepCopy(question);
+			//插入item
+			item.questions.splice(order, 0, new_question);
+
+			//改变item state
+			this.props.changeItemState();
+		},
+		deleteQuestion: function() {
+			var order = this.props.order;
+			//插入item
+			item.questions.splice(order, 1);
+
+			//改变item state
+			this.props.changeItemState();
+		},
+		render: function() {
+			var self = this;
+			var item_options = this.props.itemOptions;
+			var question = this.props.question;
+			var order = this.props.order;
+			var type = question.type;
+
+			var question_stem = question.stem ? 
+				<div className="question-stem">
+					<span>{question.stem}</span>
+					<a href="javascript:void(0)" className="delete-question-a" onClick={this.deleteQuestion}>
+						<img src="/images/manage/delete-min.png" width="16" height="16" alt="删除小题" title="删除小题" />
+					</a>
+					<a href="javascript:void(0)" className="add-question-a" onClick={this.addQuestionFromCurrent}>
+						<img src="/images/manage/add-min.png" width="16" height="16" alt="添加小题" title="添加小题" />
+					</a>
+				</div> 
+				: '';
+
+			//小题做答区域
+			var answer_area;
+
+			//当item_options存在时，小题默认为单选题
+			// if(item_options) {
+			// 	//展示选项时，是否展示选项内容
+			// 	var showOption = false;
+			// 	answer_area = <SingleChoice options={item_options} questionId={id} showOption={showOption} />
+			// }else {
+			//当item_options不存在时，判断小题类型，再做相应处理
+				if(type == "SingleChoice") {
+					answer_area = <SingleChoice options={question.options} questionOrder={order} changeItemState={self.props.changeItemState} />
+				}
+				// else if(type == "SimpleAnswer") {
+				// 	answer_area = <SimpleAnswer questionId={id} />
+				// }else if(type == "BlankFilling") {
+				// 	answer_area = <BlankFilling questionId={id} />
+				// }
+			// }
+
+			return (
+				<div className="question">
+					<div className="question-content">
+						{question.stem}
+						{answer_area}
+					</div>
+				</div>
+			);
+		}
+	});
+
+	//小题Question中的答题区域部分————单选题
+	var SingleChoice = React.createClass({
+		deleteQuestionOption: function() {
+			var question_order = this.props.questionOrder;
+			//删除当前小题的一个选项
+			item.questions[question_order].options.pop();
+
+			//改变item state
+			this.props.changeItemState();
+		},
+		addQuestionOption: function() {
+			var question_order = this.props.questionOrder;
+
+			//添加一个新的选项
+			var new_question_option = item.questions[question_order].options[0];
+			item.questions[question_order].options.push(new_question_option);
+
+			//改变item state
+			this.props.changeItemState();
+		},
+		render: function() {
+			var self = this;
+			var options = this.props.options;
+
+			return (
+				<ul className="question-options">
+				{
+					options.map(function(option, i, a) {
+						return (
+							<li>
+								<span>{option}</span>
+								<a href="javascript:void(0)" className="delete-question-option" onClick={self.deleteQuestionOption}>
+									<img src="/images/manage/delete-min.png" width="16" height="16" alt="删除小题选项" title="删除小题选项" />
+								</a>
+								<a href="javascript:void(0)" className="add-question-option" onClick={self.addQuestionOption}>
+									<img src="/images/manage/add-min.png" width="16" height="16" alt="添加小题选项" title="添加小题选项" />
+								</a>
+							</li>
+						)
+					})
+				}
+				</ul>
+			)
+		}
+	});
+
+	// 显示Item
+	ReactDOM.render(
+		<Item />,
+		$('#item-area')[0]
+	);
 
 });
 
@@ -139,7 +423,7 @@ $.fn.addQuestion = function() {
 	
 	//点击以当前模板添加小题时，添加小题 
 	$add_question.click(function(){
-		$(this).addQuestionFormCurrent();
+		$(this).addQuestionFromCurrent();
 	});
 
 	//默认添加四个选项
@@ -162,7 +446,7 @@ $.fn.addQuestion = function() {
 };
 
 //以当前小题为模板添加小题
-$.fn.addQuestionFormCurrent = function() {
+$.fn.addQuestionFromCurrent = function() {
 	var $new_question = $(this).closest('.question').clone(true);
 	//在当前小题后添加新题
 	$new_question.insertAfter($(this).closest('.question'));
