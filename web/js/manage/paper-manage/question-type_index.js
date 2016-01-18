072e752ccb833a3583108a385a49130aa3181825
dst_program <1258632526@qq.com>
2016-01-18 14:48:44 +0800

    update type show

--- web/js/manage/paper-manage/question-type_index.js
+++ web/js/manage/paper-manage/question-type_index.js
@@ -1,28 +1,5 @@
 $(function(){
-
   //删除操作
   singleDelete('question_type');
-
-
-
-
-	
-	function getStructureDate(){
-		$.ajax({
-			url: '',
-			
-			datatype:'json',
-			success: function(data) {
-				if(data.success){
-
-				}else{
-
-				}
-			},
-			error: function(XMLHttpRequest, textStatus, errorThrown) {
-
-			}
-		})
-	}
 
 });
--- web/js/manage/paper-manage/question-type_show.js
+++ web/js/manage/paper-manage/question-type_show.js
@@ -5,165 +5,67 @@ $(function(){
 
 	//首先取到要渲染的id
 	var questiontypeId= $('.question_type_sample').attr('id');
+	//取到相应的json结构
 
-	Array.prototype.shuffle = function() {
-      var newArr = [];
-      for( var i = 0, len = this.length; i < len; i++ ){
-        newArr.push(this[i]);
-      }
-      for( var i = 0, newLen = newArr.length; i < newLen; i++ ){
-        var random = Math.floor( Math.random() * newLen );
-        var temp = newArr[random];
-        newArr[random] = newArr[i];
-        newArr[i] = temp;
-      }
-      console.log(newArr);
-      return newArr;
-    }
-    /*这里有一个分开*/
-	var capitalLetterArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	var questionTypeStructureId=$('.question_type_structure').attr('id');
 
-    var PartOrderNum = 1;
-    var QuestionOrderNum = 1;
+	var item2=$('#'+questionTypeStructureId).text();
+	var item1=JSON.parse(item2);
+       /*var item = {
+	       "flowable":false,
+	       "shuffle":true,
+	       "stem":"此处为试题题干内容，题干中可以包括图片、音频以及视频等多媒体。",
+	       "show-stem-length":false,
+	       "questions-num-limit":true,
+	       "pre-show":true,
+	       "questions":	[
+	       		{
+	       			"type":"SingleChoice",
+	       			"stem":"此处为单选小题题干，题干内容可以包括图片、音频以及视频等多媒体。",
+		       		"options":[
+		       			"单选小题选项内容",
+		       			"单选小题选项内容",
+		       			"单选小题选项内容",
+		       			"单选小题选项内容"
+		       			]
+		       		}
+	       		]
+       	}*/
 
-    window.answer = localStorage['answer'] ? JSON.parse(localStorage['answer']) : {};
+    //显示每个question里面的选项
+    var Options = React.createClass({
+    	render: function(){
+    		var options=this.props.options;
 
-    var item={
-          "id": 1,
-          "stem": "there are four choice ,please choose the best one",
-          "questions":[
-             {
-               "type": "SingleChoice",
-               "shuffle": true,
-               "options": [
-                   "The man is not good at balancing his budget.",
-                   "She will go purchase the gift herself.",
-                   "The gift should not be too expensive.",
-                   "They are gonging to Jane's house-warming party."
-                 ],
-                 "strict": true,
-               "reference-answer": 0,
-               "answer-analysis": "答案解析"
-             }
-           ]
-       }
-
-    
-    //小题Question中的答题区域部分————单选题
-    var SingleChoice = React.createClass({
-      handleChange: function(event) {
-        var question_id = this.props.questionId;
-        var newValue = event.target.checked ? event.target.value : '';
-
-        answer[question_id] = newValue;
-        //  //将答案存入localstorage  
-        localStorage.setItem('answer', JSON.stringify(answer));       
-      },
-      render: function() {
-        var self = this;
-        var options = this.props.options;
-        var question_id = this.props.questionId;
-        var show_option = this.props.showOption;
-        var answer_area = [];
-        var question_answer = answer[question_id] || '';
-
-        var answer_area = options.map(function(option, i, a) {
-          return (
-            <label className={'question-option ' + ( show_option ? 'block' : 'inline' )}>
-              <input type="radio" name={question_id} value={option} defaultChecked={question_answer == option} onChange={self.handleChange} />
-              <span className={'option' + ( show_option ? ' show-option' : ' hide-option' )}>
-                <span className="option-content">{show_option ? option : ''}</span>
-              </span>
-            </label>
-          )
-        });
-        /****************选项是否打乱顺序尚未做处理*********************/
-        return (
-          <div className="answer-area">
-            {show_option ? answer_area.shuffle() : answer_area}
-          </div>
-        )
-      }
+    	var option=options.map(function(option,i,options){
+    		return (<div className="option">{option}</div>)
+    	})
+    	return (
+    		<div>
+    		{option}
+    		</div>
+    		);
+    	}
+    	
     });
-
-    //小题Question中的答题区域部分————填空题
-    var BlankFilling = React.createClass({
-      handleChange: function(event) {
-        var question_id = this.props.questionId;
-        var newValue = event.target.value;
-        answer[question_id] = newValue;
-        //  //将答案存入localstorage  
-        localStorage.setItem('answer', JSON.stringify(answer));
-      },
-      render: function() {
-        var question_id = this.props.questionId;
-        var question_answer = answer[question_id] || '';
-        var answer_area = <input type="text" placeholder="请输入答案" defaultValue={question_answer} onChange={this.handleChange} />;
-        return (
-          <div className="answer-area">
-            {answer_area}
-          </div>
-        )
-      }
-    });
-
-    //小题Question中的答题区域部分————简答题
-    var SimpleAnswer = React.createClass({
-      handleChange: function(event) {
-        var question_id = this.props.questionId;
-        var newValue = event.target.value;
-        answer[question_id] = newValue;
-        //  //将答案存入localstorage  
-        localStorage.setItem('answer', JSON.stringify(answer));
-      },
-      render: function() {
-        var question_id = this.props.questionId;
-        var question_answer = answer[question_id] || '';
-        var answer_area = <textarea placeholder="请输入答案" defaultValue={question_answer} onChange={this.handleChange}></textarea>;
-        return (
-          <div className="answer-area">
-            {answer_area}
-          </div>
-        )
-      }
-    });
-
     //试题item中包含的小题Question
     var Question = React.createClass({
       render: function() {
         var item_options = this.props.itemOptions;
         var question = this.props.question;
-        var id = this.props.id;
         var type = question.type;
 
         var question_stem = question.stem 
           ? <div className="question-stem">{question.stem}</div> 
           : '';
-        var answer_area;
-
-        //当item_options存在时，小题默认为单选题
-        if(item_options) {
-          //展示选项时，是否展示选项内容
-          var showOption = false;
-          answer_area = <SingleChoice options={item_options} questionId={id} showOption={showOption} />
-        }else {
-        //当item_options不存在时，判断小题类型，再做相应处理
-          if(type == "SingleChoice") {
-            //展示选项时，是否展示选项内容
-            var showOption = true;
-            answer_area = <SingleChoice options={question.options} questionId={id} showOption={showOption} />
-          }else if(type == "SimpleAnswer") {
-            answer_area = <SimpleAnswer questionId={id} />
-          }else if(type == "BlankFilling") {
-            answer_area = <BlankFilling questionId={id} />
-          }
-        }
 
         return (
-          <div id={id} className="question">
+          <div className="question">
             <div className="question-content">
               {question_stem}
-              {answer_area}
+            </div>
+            <div className="question-option">
+            	<Options options={question.options} />
             </div>
           </div>
         );
@@ -175,11 +77,10 @@ $(function(){
       render: function() {
         var item_options = this.props.itemOptions;
         var questions = this.props.questions;
-        var item_id = this.props.itemId;
 
         var questionsArr = questions.map(function( question, i, questions ){
           return (
-            <Question question={question} itemOptions={item_options} id={item_id + '_' + i} />
+            <Question question={question} itemOptions={item_options}/>
           );
         });
         return (
@@ -193,12 +94,8 @@ $(function(){
     //试题item题干
     var ItemStem = React.createClass({
       render: function() {
-        //题干的长度
-        var length = this.props.length ? '(' + this.props.length + " words)" : "";
-        //判断item的题干长度是否显示
-        var show_length = this.props.showLength ? true : false;
         var stem = {
-          '__html': this.props.stem + (show_length ? length : '')
+          '__html': this.props.stem
         }
         return (
           <div className="item-stem" dangerouslySetInnerHTML={stem}></div>
@@ -255,16 +152,16 @@ $(function(){
 
         return (
           <div className="item">
-            <ItemStem stem={item.stem} length={item.length} showLength={item.showLength} />
+            <ItemStem stem={item.stem}/>
             {item_options}
-            <Questions questions={item.questions} itemOptions={options} itemId={item.id} /> 
+            <Questions questions={item.questions} itemOptions={options} /> 
           </div>
         );
       }
     });
     
     ReactDOM.render(
-      <Item item={item} />,
+      <Item item={item1} />,
       document.getElementById(questiontypeId)
     );
 
