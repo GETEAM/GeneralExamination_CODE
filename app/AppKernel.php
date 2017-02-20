b3471e619ca1273b7b8e3e42efa3ea825aab27bc
wangxin1990 <825249167@qq.com>
2017-02-20 17:28:55 +0800

    add logged user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,8 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Ddeboer\DataImportBundle\DdeboerDataImportBundle(),
             new PaperManageBundle\PaperManageBundle(),
+            new Gos\Bundle\WebSocketBundle\GosWebSocketBundle(),
+            new Gos\Bundle\PubSubRouterBundle\GosPubSubRouterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
